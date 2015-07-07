f8fa744867ac294b211683fd738f219a8d942a7d
FauadAnwar <fauad.anwar@M-3006.local>
2015-07-07 09:36:45 +0530

    Initial Commit

---
+++ EShopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EShopping
+//
+//  Created by FauadAnwar on 07/07/15.
+//  Copyright (c) 2015 Fauad Anwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
